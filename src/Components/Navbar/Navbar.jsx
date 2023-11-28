09c2e20ab241383dd932087a8504d5fc724638c6
AN-TANVIR-LENOVORV20 <Array@TANVIR.arraylab.in>
2023-11-29 01:47:12 +0530

    hy

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,31 @@
 import { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
+
+import Slide from "react-reveal/Slide";
 import { Link } from "react-router-dom";
+import { Fade } from "react-reveal";
+
+import { CiViewTimeline } from "react-icons/ci";
 import navnew from "../../Images/Navnew.svg";
 export const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 600);
+  const [mobNav, setmobNav] = useState(false);
+  const handlemobNav = () => {
+    setmobNav(!mobNav);
+  };
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 600);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
@@ -14,6 +35,10 @@ export const Navbar = () => {
     setShowMenu(false);
   };
   useEffect(() => {
+    function isMobileScreen() {
+      console.log(window.innerWidth <= 800);
+      return window.innerWidth <= 800;
+    }
     const onScroll = () => {
       const scrollY = window.scrollY;
       setScrollPosition(scrollY);
@@ -25,6 +50,7 @@ export const Navbar = () => {
   }, []);
 
   return (
+    <>
     <nav
       className={styles.navbar}
       style={{
@@ -40,7 +66,7 @@ export const Navbar = () => {
           />
         </Link>
       </li>
-
+      {!isMobile ? (<div>
       <div className={styles["navbar-toggle"]} onClick={toggleMenu}>
         <i className="fas fa-bars"></i>
       </div>
@@ -58,10 +84,10 @@ export const Navbar = () => {
         </li>
         <li>
           <div className={styles.dropdown1}>
-            <span style={{fontSize:'18px' ,fontWeight:'500'}}>Our Brand</span>
+            <span style={{ fontSize: "18px", fontWeight: "500" }}>
+              Our Brand
+            </span>
             <div className={styles["dropdown-content1"]}>
-              
-              
               <p className="text-hover">
                 {" "}
                 <Link to="/Hitechmart" onClick={closeMenu}>
@@ -101,13 +127,10 @@ export const Navbar = () => {
             <div className={styles.dropdown3}>
               <span>IT Services</span>
               <div className={styles["dropdown-content3"]}>
-              <div className="para-nav">
-                <p>
-                
-                  <Link to="/ServiceUpperPart" onClick={closeMenu}>
-                    Software Development
-                  </Link>
-                </p>
+                <div className="para-nav">
+                  <p onClick={closeMenu}>
+                    <Link to="/ServiceUpperPart">Software Development</Link>
+                  </p>
                 </div>
                 <p>
                   <Link to="/WebDevelopment" onClick={closeMenu}>
@@ -173,11 +196,6 @@ export const Navbar = () => {
                 </p>
               </div>
             </div>
-            {/* <p>
-      <Link to="brandPage4" onClick={closeMenu}>
-        Job Tanks
-      </Link>
-    </p> */}
             <div className={styles.dropdown3}>
               <span>Digital Marketing</span>
               <div className={styles["dropdown-content3"]}>
@@ -216,6 +234,167 @@ export const Navbar = () => {
           </Link>
         </li>
       </ul>
-    </nav>
+      </div>
+     ) : (
+      <CiViewTimeline className="toggler" onClick={handlemobNav} />
+    )}
+  </nav>
+  {mobNav ? (
+    <Slide top>
+      <div className="mobNavbar">
+        <div>
+          <Link to="/">
+            <div>Home</div>
+          </Link>
+        </div>
+        <div>
+          <Link to="/About">
+            <div>About</div>
+          </Link>
+        </div>
+        <div
+          // onMouseEnter={handleMouseEntermob}
+          // onMouseLeave={handleMouseLeavemob}
+        >
+          <h3>Our Brand</h3>
+          <ul
+            className="listMob"
+            // style={
+            //   isDropdownVisiblemob
+            //     ? { display: "flex" }
+            //     : { display: "none" }
+            // }
+          >
+            <Link to="/Hitechmart">
+              <li>Hitecmart</li>
+            </Link>
+            <Link to="/FranchiseTank">
+              <li>FranchiseTank</li>
+            </Link>
+            <Link to="/Heaven">
+              <li>Grand havens</li>
+            </Link>
+            <Link to="/Grand">
+              <li>Job Tanks</li>
+            </Link>
+            <Link to="/Exports">
+              <li>Certon exports</li>
+            </Link>
+          </ul>
+        </div>
+        <div
+          // onMouseEnter={handleMouseEntermob1}
+          // onMouseLeave={handleMouseLeavemob1}
+        >
+          <h3>Our Service</h3>
+          <ul
+            className="listMob1"
+            // style={
+            //   isDropdownVisiblemob1
+            //     ? { display: "flex" }
+            //     : { display: "none" }
+            // }
+          >
+            <div >
+              IT Services
+              <div
+                // style={itmouse ? { display: "flex" } : { display: "none" }}
+              >
+                <ul className="list2">
+                  <Link to="/ServiceUpperPart">
+                    <li>Software Development</li>
+                  </Link>
+                  <Link to="/WebDevelopment">
+                    <li>Web Development </li>
+                  </Link>
+                  <Link to="/AppDevelopment">
+                    <li>App Development</li>
+                  </Link>
+                </ul>
+              </div>
+            </div>
+            <div >
+              <li>
+                HR Services
+                <Fade>
+                  <div
+                    // style={
+                    //   hrmouse ? { display: "flex" } : { display: "none" }
+                    // }
+                  >
+                    <ul className="list2">
+                      <Link to="/Hr">
+                        <li>Talent Acquisition/Recruitment</li>
+                      </Link>
+                      <Link to="/PerformanceManagement">
+                        <li>Performance Management</li>
+                      </Link>
+                      <Link to="/TrainingAndDevelopment">
+                        <li>Employee Training/Development</li>
+                      </Link>
+                      <Link to="/Development">
+                        <li>Organizational Development</li>
+                      </Link>
+                    </ul>
+                  </div>
+                </Fade>
+              </li>
+            </div>
+            <div >
+              <li>
+                Sales and Marketing
+                <div
+                  
+                >
+                  <ul className="list2">
+                    <Link to="/Branding">
+                      <li>Branding & Position</li>
+                    </Link>
+                    <Link to="/Market">
+                      <li>Market Research & Analysis</li>
+                    </Link>
+                    <Link to="/Crm">
+                      <li>CRM</li>
+                    </Link>
+                    <Link to="/SalesSupport">
+                      <li>Sales Support</li>
+                    </Link>
+                  </ul>
+                </div>
+              </li>
+            </div>
+            <div >
+              <li>
+                Digital Marketing
+                <div>
+                  <Fade>
+                    <div
+                      
+                    >
+                      <ul className="list2">
+                        <Link to="/SearchEngine">
+                          <li>Search Engine Optimization</li>
+                        </Link>
+                        <Link to="/Pay">
+                          <li>pay Per Click Advertising</li>
+                        </Link>
+                        <Link to="/Media">
+                          <li>Social Media Marketing</li>
+                        </Link>
+                        <Link to="/Email">
+                          <li>E-mail Marketing</li>
+                        </Link>
+                      </ul>
+                    </div>
+                  </Fade>
+                </div>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    </Slide>
+  ) : null}
+  </>
   );
 };
